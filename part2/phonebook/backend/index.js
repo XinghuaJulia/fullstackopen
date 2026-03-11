16a69d3c3bbebc6ba63e862b08a6adfdaac2c4a4
Uselesstrash06 <uselesstrash06@gmail.com>
2026-03-11 10:03:14 +0800

    Starting part 3

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
@@ -109,7 +113,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
